fb9f6284b6966f1ca0a770ed570b4ca486bbaaf0
Prosenjit Swarnakar <prosenjitswarnakar2002@gmail.com>
2022-10-14 23:48:17 +0530

    Category added

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -8,7 +8,39 @@ class News {
   List<ArticleModel> news = [];
 
   Future<void> getNews() async {
-    String url = "https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=0be6096aa54449e88da2f202525da8bd";
+    String url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=0be6096aa54449e88da2f202525da8bd";
+
+    var response = await http.get(Uri.parse(url));
+
+    var jsonData = jsonDecode(response.body);
+    if (jsonData['status'] == "ok") {
+      jsonData['articles'].forEach((element) {
+        if (element["urlToImage"] != null && element['description'] != null) {
+          ArticleModel articleModel = ArticleModel(
+              title: element['title'],
+              author: element["author"] ?? "",
+              description: element["description"],
+              url: element["url"],
+              urlToImage: element["urlToImage"],
+              content: element["content"] ?? ""
+          );
+
+          news.add(articleModel);
+
+
+
+        }
+      });
+    }
+  }
+}
+
+class CategoryNewsClass {
+
+  List<ArticleModel> news = [];
+
+  Future<void> getNews(String category) async {
+    String url = "https://newsapi.org/v2/top-headlines?country=in&category=$category&apiKey=0be6096aa54449e88da2f202525da8bd";
 
     var response = await http.get(Uri.parse(url));
 
